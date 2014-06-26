7236212f064d5df79a0b17f42af0a85d48d9f964
nandan <nandan@sharma-pc.Home>
2014-06-26 11:47:26 -0700

    Added 3 methods in ListUtils.java

--- PasswordApp/src/main/java/com/home/pms/common/utils/ListUtils.java
+++ PasswordApp/src/main/java/com/home/pms/common/utils/ListUtils.java
@@ -34,4 +34,37 @@ public class ListUtils {
 	public static boolean isEmptyList(final List<?> refList) {
 		return (refList == null) || (refList.size() == 0) ? true : false;
 	}
+	
+	/**
+	 * @param refList
+	 * @return
+	 */
+	public Object getFirstElement(final List<?> refList) {
+		if (refList != null || !refList.isEmpty()) {
+			return refList.get(0);
+		}
+		return null;
+	}
+	
+	/**
+	 * @param refList
+	 * @return
+	 */
+	public Object getLastElement(final List<?> refList) {
+		if (refList != null || !refList.isEmpty()) {
+			return refList.get(refList.size()-1);
+		}
+		return null;
+	}
+	/**
+	 * @param refList
+	 * @return
+	 */
+	public	String[] convertToStringArray(final List<?> refList){
+		if (refList != null || !refList.isEmpty()) {
+			String[] strResult=new String[refList.size()];  
+			refList.toArray(strResult);
+		}
+		return null;
+	}
 }
